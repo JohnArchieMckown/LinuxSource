b6f4074db733dbc78c3093fecde5587d4f5d009c
john.archie.mckown <john.archie.mckown@gmail.com>
2024-06-15 12:58:24 -0500

    used rustfmt on template.rs

--- template.rs
+++ template.rs
@@ -1,18 +1,18 @@
 use std::env;
 
 fn main() {
-    let args = env::args();
-    let envs = env::vars();
+    let args = env::args(); // This is an iterator
+    let envs = env::vars(); // This is also an iterator
 
-    println!("Arguments:");
+    println!("\nArguments:");
     for i1 in args {
         println!("> {}", i1);
     }
 
-    println!("Environment variables:");
+    println!("\nEnvironment variables:");
     for i2 in envs {
         println!("> {}={}", i2.0, i2.1);
     }
-//    dbg!(args);
-//    dbg!(envs);
+    //    dbg!(args);
+    //    dbg!(envs);
 }
