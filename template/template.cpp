7a4388ffb34b777c6fb3c454981ea5c41d3a124b
john.archie.mckown <john.archie.mckown@gmail.com>
2024-06-29 14:23:08 -0500

    removed unneeded comments

--- template/template.cpp
+++ template/template.cpp
@@ -10,17 +10,10 @@ int main (int argc, char *argv[], char *envp[]) {
 	int index=0;
 	vector<string> arguments(argv, argv+argc);
 
-	assert(arguments.size() == argc);
-
 	cout << "There are " + to_string(argc) + " arguments"; cout << endl;
-//	for (index=0; index<argc; index++) {
-//		cout << to_string(index) + ":" + argv[index]; cout << endl;
-//		}
-
-	assert(arguments.size() == argc);
-
-//	for(index=0; index<argc; index++) { arguments.push_back(argv[index]); }
-
-	for(string argument : arguments) { cout << to_string(index++) + ":\t" +argument; cout << endl; }
+	for(string argument : arguments) { 
+		cout << to_string(index++) + ":\t" +argument; 
+		cout << endl; 
+	}
 	return 0;
 }
