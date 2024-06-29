70a2ac22fae0801d94319f1bcd84e83cb2952e19
john.archie.mckown <john.archie.mckown@gmail.com>
2024-06-29 14:08:56 -0500

    even more "c++"ish

--- template/template.cpp
+++ template/template.cpp
@@ -7,20 +7,20 @@
 using namespace std;
 
 int main (int argc, char *argv[], char *envp[]) {
-	int index;
+	int index=0;
 	vector<string> arguments(argv, argv+argc);
 
 	assert(arguments.size() == argc);
 
-	cout << "There are "; cout << argc; cout << " arguments"; cout <<endl;
+	cout << "There are " + to_string(argc) + " arguments"; cout << endl;
 //	for (index=0; index<argc; index++) {
-//		cout << index + ":" + argv[index]; cout << endl;
+//		cout << to_string(index) + ":" + argv[index]; cout << endl;
 //		}
 
 	assert(arguments.size() == argc);
 
 //	for(index=0; index<argc; index++) { arguments.push_back(argv[index]); }
 
-	for(string argument : arguments) { cout << argument <<endl; }
+	for(string argument : arguments) { cout << to_string(index++) + ":\t" +argument; cout << endl; }
 	return 0;
 }
