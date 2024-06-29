8b45e3e6d121df628bbc7a58ed9856e7403c2e01
john.archie.mckown <john.archie.mckown@gmail.com>
2024-06-29 13:50:11 -0500

    make it more "cpp"ish

--- template/template.cpp
+++ template/template.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <array>
+#include <string>
+#include <vector>
+#include <cassert>
 
 using namespace std;
 
 int main (int argc, char *argv[], char *envp[]) {
 	int index;
+	vector<string> arguments(argv, argv+argc);
 
-	cout << "There are "; cout << argc; cout << " arguments."; cout <<endl;
-	for (index=0; index<argc; index++) {
-		cout << argv[index]; cout << endl;
-		}
+	assert(arguments.size() == argc);
+
+	cout << "There are "; cout << argc; cout << " arguments"; cout <<endl;
+//	for (index=0; index<argc; index++) {
+//		cout << index + ":" + argv[index]; cout << endl;
+//		}
+
+	assert(arguments.size() == argc);
+
+//	for(index=0; index<argc; index++) { arguments.push_back(argv[index]); }
+
+	for(string argument : arguments) { cout << argument <<endl; }
 	return 0;
 }
