a43c6ed29dc4fbfc90e16d62c9e6cef34ba21a1e
john.archie.mckown <john.archie.mckown@gmail.com>
2024-07-03 18:25:58 -0500

    chaning testConsole to QCoreApplication & Qt functions instead of normal C++ code, for learning

--- qt/testConsole/main.cpp
+++ qt/testConsole/main.cpp
@@ -1,6 +1,7 @@
 #include <QtCore/QCoreApplication>
 #include <QtCore/qglobal.h>
 
+#include <QtCore/QProcessEnvironment>
 #include <iostream>
 
 using namespace std;
@@ -9,8 +10,25 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    int pid = a.applicationPid();
+    const int pid = a.applicationPid();
+    vector<string> arguments(argv, argv + argc);
+    const QStringList environment = QProcessEnvironment::systemEnvironment().toStringList();
+    QStringList args;
+    QList<string> myList;
+
+    myList.reserve(arguments.size());
+    std::copy(arguments.begin(), arguments.end(), std::back_inserter(myList));
+
     std::cout << "Running with pid=" << pid << endl;
+    std::cout << endl << "Environment Variables" << endl;
+    for (QString env : environment) {
+        cout << env.toStdString() << endl;
+    }
+
+    std::cout << endl << "Program arguments (" << myList.size() << "):" << endl;
+    for (string arg : myList) {
+        cout << arg << endl;
+    }
 
     return 0; // a.exec(); the a.exec() put in by qtcreator is wrong.
 }
