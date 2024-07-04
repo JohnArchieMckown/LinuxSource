48369abadca11aab2bd6259c8771330303cf3df4
john.archie.mckown <john.archie.mckown@gmail.com>
2024-07-04 16:53:26 -0500

    replace std::endl with '\n'

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main (int argc, char *argv[], char *envp[]) {
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+    std::cout << "Hello World!" << '\n';
+    std::cout.flush(); // because I'm OCD
+    return 0;
 }
--- template/template.cpp
+++ template/template.cpp
@@ -8,9 +8,9 @@ int main (int argc, char *argv[], char *envp[]) {
 	int index=0;
 	std::vector<std::string> arguments(argv, argv+argc);
 
-	std::cout << "There are " + std::to_string(argc) + " arguments" << std::endl;
+	std::cout << "There are " + std::to_string(argc) + " arguments" << '\n';
 	for(std::string argument : arguments) { 
-		std::cout << std::to_string(index++) + ":\t" +argument << std::endl; 
+		std::cout << std::to_string(index++) + ":\t" +argument << '\n'; 
 	}
 	return 0;
 }
