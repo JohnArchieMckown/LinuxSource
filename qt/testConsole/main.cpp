42aaa591249ceb1a250276028d557d690e70bc25
john.archie.mckown <john.archie.mckown@gmail.com>
2024-07-08 16:55:22 -0500

    update main.cpp with a comment that "a.exec()" is not needed unless the event loop needs to run, which is does not in this case.
    update .gitignore to ignore *.pro.user since that is regenerated as
    needed by the associated *.pro file.

--- qt/testConsole/main.cpp
+++ qt/testConsole/main.cpp
@@ -2,7 +2,7 @@
 #include <QtCore/qglobal.h>
 
 #include <QTextStream>
-#include <QTimer>
+//#include <QTimer>
 #include <QtCore/QProcessEnvironment>
 
 int main(int argc, char *argv[])
@@ -28,6 +28,6 @@ int main(int argc, char *argv[])
     }
     output.flush(); // likely not needed, but I'm OCD
 
-    QTimer::singleShot(1, &a, [] { exit(0); });
-    return a.exec(); // the a.exec() put in by qtcreator is wrong.
+    //  QTimer::singleShot(1, &a, [] { exit(0); });
+    return 0; // a.exec(); the a.exec() is only needed for some async services.
 }
