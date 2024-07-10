8ecdb7fcb90d121fcf725d776e6b924046f1f935
john.archie.mckown <john.archie.mckown@gmail.com>
2024-07-10 17:21:01 -0500

    added class with operator= override, lots of learning & screaming

--- template/template.cpp
+++ template/template.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
-#include <array>
 #include <string>
 #include <vector>
-#include <cassert>
+#include <memory>
+#include <assert.h>
+
+class Mine {
+	public:
+
+	Mine() {
+		std::cout << "Mine() constructor" << std::endl;
+//		m_mine = std::make_unique<Mine>()
+	};
+
+	~Mine() {
+		std::cout << "Mine() destructor" << std::endl;
+	};
+
+	Mine& operator=(const Mine& lhs) {
+		std::cout << "Mine() operator=" << std::endl;
+		if (this == &lhs) {
+			return *this;
+		}
+		this->m_mine = lhs.m_mine;
+		return *this ;
+	}
+	
+//	std::unique_ptr<int> m_mine;
+	int m_mine;
+};
 
 int main (int argc, char *argv[], char *envp[]) {
 	int index=0;
@@ -12,5 +37,18 @@ int main (int argc, char *argv[], char *envp[]) {
 	for(std::string argument : arguments) { 
 		std::cout << std::to_string(index++) + ":\t" +argument << '\n'; 
 	}
+	
+	Mine x1; // = new Mine();
+	x1.m_mine = 1234;
+	Mine x2; // = new Mine();
+        x2 = x1;
+	x2.m_mine = 4321;
+//	assert(&x2->m_mine != &x1->m_mine);
+
+	std::cout << "x1:" << x1.m_mine << " " << &x1.m_mine << " " << &x1 << std::endl;
+	std::cout << "x2:" << x2.m_mine << " " << &x2.m_mine << " " << &x2 << std::endl;
+//	delete x1;
+//	delete x2;
+
 	return 0;
 }
