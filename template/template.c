73c1e0b29a3902755fde2137c73b5f4b2dccb94a
john.archie.mckown <john.archie.mckown@gmail.com>
2024-07-10 16:09:47 -0500

    add some version information from GCC's builtins

--- template/template.c
+++ template/template.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define xstr(s) str(s)
+#define str(s) #s
+// IMO, I don't really like using xstr, but I copied it from the manual.
+const char version[]="version:" xstr(__VERSION__) "\n";
+const char stdc_version[]="stdVersion:" xstr(__STDC_VERSION__) "\n";
+
 int main(int argc, char *argv[], char *envp[]) {
 	int envp_count=0;
 	char **envp_=envp;
@@ -15,7 +22,7 @@ int main(int argc, char *argv[], char *envp[]) {
 	printf("\nThere %s %d environment variable%s\n",(1==envp_count)?is:are,envp_count,(1==envp_count)?"":"s");
 	printf("There %s %d argument%s\n",(1==argc)?is:are,argc,(1==argc)?"":"s");
 	for (int i=0; i<argc; i++) {
-		printf("Argument %d is %d bytes:%s\n",i,strlen(argv[i]), argv[i]);
+		printf("Argument %d is %lu bytes:%s\n",i,strlen(argv[i]), argv[i]);
 	}
 }
 		
