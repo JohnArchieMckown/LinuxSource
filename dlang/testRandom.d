537d047617d77af6cbf3e7fc77242dbd7846bb18
john.archie.mckown <john.archie.mckown@gmail.com>
2024-07-21 17:43:12 -0500

    D language, just messing around

--- dlang/testRandom.d
+++ dlang/testRandom.d
@@ -11,17 +11,14 @@ void main() {
 	auto countOfValue = new int[100]; 
 	auto rnd = Random(unpredictableSeed);
 	for(int i=0;i<numberOfTests;i++) {
-		auto idx = uniform(0, 100, rnd); // generates 0..99, half-closed half-open interval if you're interested
-		countOfValue[idx]++;
+		auto idx = uniform(0, 100, rnd); // generates 0..99, half-closed half-open interval from 0 to 100, if you're interested.
+		countOfValue[idx]++;			// how many times has this number been generated?
 	}
 	write("Count of values:");writeln(countOfValue);
 	write("Sum of Values:"); writeln(sum(countOfValue));
 	int max=0, min=int.max;
-//	for(int idx=0;idx<countOfValue.length;++idx) {
 	foreach(int value; countOfValue) {
-//		max = max < countOfValue[idx] ? countOfValue[idx] : max;
 		max = max < value ? value : max;
-//		min = min > countOfValue[idx] ? countOfValue[idx] : min;
 		min = min > value ? value : min;
 	}
 	write("Range of values is from ");write(min);write(" to ");write(max);
@@ -29,5 +26,5 @@ void main() {
 	write(" which is a percentage difference of ");write(100.0*to!float(max - min)/numberOfTests);writeln("%");
 	write("Max of values:");writeln(max);
 	write("Min of values:");writeln(min);
-	assert(sum(countOfValue) == numberOfTests);
+	assert(sum(countOfValue) == numberOfTests); // better be! Think about it, if you're confused.
 }
