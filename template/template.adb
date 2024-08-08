b3f95023e59febccbcf8bb36f88baf01981f4206
john.archie.mckown <john.archie.mckown@gmail.com>
2024-08-08 14:57:10 -0500

    sync

---
+++ template/template.adb
@@ -0,0 +1,9 @@
+-- Ada (gnat) program to print arguments
+-- and all environment variables
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Command_Line; use Ada.Command_line
+procedure Template is
+begin
+	
+end Template;
+
